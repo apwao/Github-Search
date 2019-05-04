d002778f3780c6facf9b75b5b11887c8dca4e5bd
Sowayi <sapwao@gmail.com>
2019-05-04 18:57:16 -0400

    Add a component for User details

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,4 @@
 export class User {
+    constructor(public userName:string, public briefDescription:string, public image:string, public myReposUrl:string, public myPublicRepos:number ,public fullName?:string){   
+    }
 }
