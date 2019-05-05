66224cc8cd3778c0b83828f41fb6b7a6e9b2ea47
Sowayi <sapwao@gmail.com>
2019-05-05 04:56:34 -0400

    Add form to receive user input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { UserInfoService } from '../user-http/user-info.service';
+import { User } from '../user';
+import { Repo } from '../repo';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  providers: [UserInfoService]
 })
 export class UserComponent implements OnInit {
+  
+  userFinalDetails:User;
+  userFinalRepoDetails: Repo;
 
-  constructor() { }
+  constructor(public userInfoService:UserInfoService) { }
 
   ngOnInit() {
+    // this.userInfoService.userDetailsRequest();
+    // this.userFinalDetails = this.userInfoService.userDetails;
   }
 
 }
