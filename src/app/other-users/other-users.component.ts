5bba935392fa4ab76db45e56d6c50f3f9f59aeef
Sowayi <sapwao@gmail.com>
2019-05-05 04:55:40 -0400

    Add functionality to display user details

--- src/app/other-users/other-users.component.ts
+++ src/app/other-users/other-users.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {UserInfoService} from '../user-http/user-info.service';
 
 @Component({
   selector: 'app-other-users',
   templateUrl: './other-users.component.html',
-  styleUrls: ['./other-users.component.css']
+  styleUrls: ['./other-users.component.css'],
+  providers: [UserInfoService]
 })
 export class OtherUsersComponent implements OnInit {
+  userNameInput: string = "Apwao";
+  userFinalDetails;
+   getUserInfo(apwao){
+    // alert(this.userNameInput)
+     this.userInfoService.userDetailsRequest(this.userNameInput);
+      this.userFinalDetails=this.userInfoService.userDetails
 
-  constructor() { }
+   }
+
+  constructor(public userInfoService:UserInfoService) { }
 
   ngOnInit() {
   }
