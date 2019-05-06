5331497cb71228d01392d8e58e0c90be51042eb2
Sowayi <sapwao@gmail.com>
2019-05-06 10:43:02 -0400

    Add API details to environment.prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  myApiUrl:"https://api.github.com/users/apwao?access_token=4cb35c19a85fa8aec12c3bf35414893c722233f3",
+  userApi1Url: "https://api.github.com/users/",
+  userApi2Url: "?acess_token=4cb35c19a85fa8aec12c3bf35414893c722233f3",
+  myRepoApiUrl: "https://api.github.com/users/apwao/repos",
+  otherRepoApi1Url: "https://api.github.com/users/",
+  otherRepoApi2Url: "/repos"
 };
