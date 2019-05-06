7b6e90512ba0b30208ae4bd2fc9705de0e64c046
Sowayi <sapwao@gmail.com>
2019-05-06 08:28:32 -0400

    Add route to redirect back to homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OtherUsersComponent } from './other-users/other-users.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"other_users", component:OtherUsersComponent },
+  {path : "user", component:UserComponent},
+  {path: "" , redirectTo:"/user", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { };
+export const routingComponents = [
+  OtherUsersComponent, UserComponent
+]
